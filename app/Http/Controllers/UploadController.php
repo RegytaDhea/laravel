6c915839b7701f44703f954b579a3b30ebb334fd
regytadhea <regytadhea@gmail.com>
2017-11-13 10:15:51 +0700

    push 3

--- app/Http/Controllers/UploadController.php
+++ app/Http/Controllers/UploadController.php
@@ -10,4 +10,37 @@ class UploadController extends Controller
 	{
 		return view('uploads.create');
 	}
+	
+	public function store(Request $request)
+	{
+		$this->validate($request, [
+		'name' => 'required',
+		'image' => 'required|mimes:jpeg,png,jpg',
+		'file' => 'required|mimes:pdf,doc,docx',
+		]);
+		
+		$input = $request->all();
+		
+		$imageName = '';
+		if ($request->hasFile('image')) {
+			$imageExtension = $request->file('image')->getClientOriginalExtension();
+			$imageName = 'image_'.time().'.'.$imageExtension; //file akan disimpan dg nama image_waktu upload, itu terserah, bisa namanya saja
+			$imageDestination = base_path() . '/public/uploads';
+			$request->file('image')->move($imageDestination, $imageName);
+			$input['image'] = $imageName;
+		}
+		
+		$fileName = '';
+		if ($request->hasFile('file')) {
+			$fileExtension = $request->file('file')->getClientOriginalExtension();
+			$fileName = 'file_'.time().'.'.$fileExtension;
+			$fileDestination = base_path() . '/public/uploads'; //akan disimpan di folder public/uploads
+			$request->file('file')->move($fileDestination, $fileName);
+			$input['file'] = $fileName;
+		}
+		
+		$upload = Upload::create($input);
+		return redirect()->route('upload.index')
+		->with('success','Image/file successfully added');
+	}
 }
